31e6ce5ef4c166bac6576bc5197a53a10009de3f
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-07-16 11:38:08 +0900

    웹사이트 방문 버튼

--- src/components/common/BackHome.vue
+++ src/components/common/BackHome.vue
@@ -5,11 +5,11 @@
       <metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata>
       <g><path d="M380.5,100.5l-350,350c-27.3,27.3-27.3,71.6,0,99l350,350c27.3,27.3,71.6,27.3,99,0c27.3-27.3,27.3-71.6,0-99L249,570h671c38.7,0,70-31.3,70-70c0-38.7-31.3-70-70-70H249l230.5-230.5c13.7-13.7,20.5-31.6,20.5-49.5c0-17.9-6.8-35.8-20.5-49.5C452.1,73.2,407.8,73.2,380.5,100.5z"/></g>
       </svg> -->
-      <svg version="1.1" class="w-5 dark:fill-yellow-500 fill-blue-900 mr-2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">
+      <svg version="1.1" class="w-5 dark:fill-white fill-blue-900 mr-2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">
       <metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata>
       <g><path d="M976.5,458.2H170l206-198.3c5.3-5.3,5.3-13.8,0-19.1l-41.4-41.4c-5.3-5.3-13.8-5.3-19.1,0L56.2,449c-0.3,0.3-0.6,0.5-0.9,0.8l-1.4,1.4l-40,38.5c-2.8,2.8-4.1,6.6-3.9,10.3c-0.2,3.7,1.1,7.5,3.9,10.3L54,548.9l1.3,1.3c0.3,0.3,0.5,0.5,0.8,0.7l259.3,249.6c5.3,5.3,13.8,5.3,19.1,0l41.4-41.4c5.3-5.3,5.3-13.8,0-19.1l-204-196.4h804.6c7.5,0,13.5-6,13.5-13.5v-58.5C990,464.2,984,458.2,976.5,458.2z"/></g>
       </svg>
-      <span class="dark:text-yellow-500 dark:hover:text-yellow-300 text-blue-900 hover:text-blue-700 font-bold text-xl">Home</span>
+      <span class="dark:text-white dark:hover:text-white text-blue-900 hover:text-blue-700 font-bold text-xl">Home</span>
     </router-link>
   </div>
 </template>
--- src/pages/Career.vue
+++ src/pages/Career.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="dark:text-primary text-third">
-    <BackHome class="relative"></BackHome>
+    <div class="flex justify-between">
+      <BackHome class="relative"></BackHome>
+      <div class="lg:mt-12 mt-0 relative border py-2 px-4 rounded-full dark:border-gray-100 border-third text-third dark:text-gray-100 font-bold">Visit the website</div>
+    </div>
     <div class="relative mt-4">3</div>
   </div>
 </template>
