1ac6f91dd2bd3d81f165da183f614b4fdb996093
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-07-16 11:11:05 +0900

    반응형 헤더 수정

--- src/components/HeadLine.vue
+++ src/components/HeadLine.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="self-center xl:mt-[35vh] lg:mt-80 md:mt-12 mt-12 text-right flex-col flex">
     <div style="text-align:right; text-align:-webkit-right;" >
-      <router-link :to="route.path" class="transition-all ease-in-out linkWrap block w-fit" v-for="route in routes" :key="route">
+      <router-link :to="route.path" class="transition-all ease-in-out linkWrap block w-fit lg:my-20 my-6" v-for="route in routes" :key="route">
         <div v-if="route.meta.isMenu" class="">
             <i :class="route.icon"></i> 
-            <span class="ml-5 font-light lg:inline-block navLink py-4 hearderList" :class="`${dark ? 'white_navLink' : 'dark_navLink'}`" style="font-family: 'Archivo Black', sans-serif;">{{route.title}}</span>
+            <span class="lg:ml-5 font-light lg:inline-block navLink hearderList text-5xl" :class="`${dark ? 'white_navLink' : 'dark_navLink'}`" style="font-family: 'Archivo Black', sans-serif;">{{route.title}}</span>
         </div>
       </router-link>  
     </div>
@@ -51,7 +51,6 @@ export default {
   /* .linkWrap:hover .navLink {
     color: #fff;
   } */
-  .hearderList{font-size: 5vh;}
   @screen lg {
    .hearderList{font-size: 10vh;}
   }
