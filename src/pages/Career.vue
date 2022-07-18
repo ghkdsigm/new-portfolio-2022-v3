4ddda80e0da090fcb0b57a46439ba41da0f2caff
DESKTOP-F2VQJ5V\ghkds <ghkdsigm3@gmail.com>
2022-07-18 11:43:41 +0900

    커리어 수정

--- src/pages/Career.vue
+++ src/pages/Career.vue
@@ -4,7 +4,110 @@
       <BackHome class="relative"></BackHome>
       <a href="#" class="lg:mt-12 mt-0 dark:hover:bg-amber-400 relative lg:text-lg text-xs border py-2 px-4 rounded-full dark:border-gray-100 dark:hover:border-transparent border-third text-third dark:hover:text-black dark:text-gray-100 font-bold hover:bg-third hover:text-white">Career-Description.pdf</a>
     </div>
-    <div class="relative mt-4">3</div>
+    <div class="relative lg:mt-36 mt-4 text-left">
+      <div class="lg:mt-4 block dark:text-secondary text-secondary-dark mb-20">
+        <div class="lg:flex items-center block">
+          <img src="https://mottohportfolio.shop/img/earth.eff31dfc.gif" alt="" style="mix-blend-mode: exclusion;">
+          <!--기본정보(이력)-->
+          <h4 class="lg:text-2xl text-base mb-10 font-light">
+            수많은 대국민 서비스 프로젝트에서 다양한 역할로서의 업무 수행을 통해<br/>
+            <b class="lg:text-2xl text-lg font-extrabold">성공적인 결과</b>를 이끌어낸 경험은<br /> 
+            누구도 쉽게 겪어보지못할 <b class="lg:text-2xl text-lg font-extrabold">소중한 경험</b>이라 생각하며,<br/>
+            그 순간들을 겪으며 얻은 <b class="lg:text-2xl text-lg font-extrabold">생각</b>과 <b class="lg:text-2xl text-lg font-extrabold">배움</b>을 또 다른 프로젝트에서 <b class="lg:text-2xl text-lg font-extrabold">담아내고자</b> 합니다.
+          </h4>
+        </div>
+        <div class="career mb-6">
+          <h5 class="font-bold lg:text-3xl text-xl">Companies</h5>
+          <ul>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">(現) HYPERNEST</b> 개발팀 기획 & 프론트엔드 총괄</span>
+              <span class="font-base text-sm lg:pl-4 pl-0 lg:mb-0 mb-4 lg:font-base lg:flex block">2022.05. - </span>
+            </li>     
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">(前) UNBIZ</b> 기업부설연구소 개발 팀장</span>
+              <span class="font-base text-sm pl-4">2016.12. - 21.11</span>              
+            </li>      
+          </ul>
+        </div>
+        <div class="career mb-6">
+          <h5 class="font-bold lg:text-3xl text-xl">Representive Projects</h5>
+          <ul>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[Hypernest]</b> GXG.Style NFT 거래소 구축 및 운영</span>
+              <span class="font-base text-sm pl-4">2022.05. - 22.11</span>              
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[Hypernest]</b> GotG.World 개편</span>
+              <span class="font-base text-sm pl-4">2022.05. - 22.07</span>              
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[한국프로스포츠협회]</b> 프로스포츠 정보광장 개선 및 운영</span>
+              <span class="font-base text-sm pl-4">2016.12. - 21.11</span>              
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[국가대표선수촌]</b> 국가대표선수촌 개방 확대 지원 온라인 예약구축</span>
+              <span class="font-base text-sm pl-4">2021.06. - 21.10</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[제주유나이티드]</b> 제주유나이티드 내손안의전광판 플랫폼 운영 고도화 사업</span>
+              <span class="font-base text-sm pl-4">2021.03. - 21.09</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[한국도핑방지위원회]</b> 보건의료인 도핑방지 인증제도 온라인 학습관리시스템 구축</span>
+              <span class="font-base text-sm pl-4">2020.08. - 20.12</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[한국프로스포츠협회]</b> 프로스포츠 정보광장 웹사이트 기능고도화 작업</span>
+              <span class="font-base text-sm pl-4">2020.08. - 20.12</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[경남체육회]</b> 경남청소년스포츠한마당 대회 앱 개발 및 운영 사업</span>
+              <span class="font-base text-sm pl-4">2020.10. - 20.11</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[한국도핑방지위원회]</b> 도핑검사 포털(ADTIS) 및 검사관 온라인 교육시스템 구축 사업</span>
+              <span class="font-base text-sm pl-4">2020.07. - 20.11</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[한국프로스포츠협회]</b> 프로스포츠 협회 홈페이지 윤리교육 일정관리시스템 개발 및 관리기능개선</span>
+              <span class="font-base text-sm pl-4">2020.07. - 20.08</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[한국프로스포츠협회]</b> 한국 프로스포츠 정보광장포탈 웹사이트 개선</span>
+              <span class="font-base text-sm pl-4">2020.03. - 20.06</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[대한체육회]</b> 대한체육회 국가대표 선수촌 방문예약 시스템 개선사업</span>
+              <span class="font-base text-sm pl-4">2019.11. - 20.04</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[한국프로스포츠협회]</b> 프로스포츠 암표 신고 사이트 구축</span>
+              <span class="font-base text-sm pl-4">2020.02. - 20.02</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[대한체육회]</b> 대한체육회 100주년 기념 홈페이지 개편 사업</span>
+              <span class="font-base text-sm pl-4">2019.09. - 19.12</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[대한체육회]</b> 한국 체육을 빛낸 100인 후보자 추천 홈페이지 구축 및 운영</span>
+              <span class="font-base text-sm pl-4">2019.06. - 19.11</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[대한체육회]</b> 2019 대한민국 스포츠영웅 선정절차 시스템 구축운영</span>
+              <span class="font-base text-sm pl-4">2019.07. - 19.09</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[K-LEAGUE]</b> K리그 심판교육사이트 운영위탁</span>
+              <span class="font-base text-sm pl-4">2018.03. - 18.12</span>
+            </li>
+            <li class="py-1 lg:text-lg text-sm">
+              <span class="font-base"><b class="font-base">[경찰청]</b> 사이버 경찰청 홈페이지 개편사업</span>
+              <span class="font-base text-sm pl-4">2016.12. - 17.10</span>
+            </li>
+          </ul>
+        </div>        
+      </div>
+    </div>
   </div>
 </template>
 
