3c60be71223d9fb81d3cf170c89513989213d7e2
DESKTOP-F2VQJ5V\ghkds <ghkdsigm3@gmail.com>
2022-07-20 09:45:20 +0900

    디테일페이지 타이틀 사이즈 확대

--- src/pages/works/WorksDetailNew.vue
+++ src/pages/works/WorksDetailNew.vue
@@ -5,7 +5,7 @@
       <a :href="data.url" target="_blank" class="lg:mt-12 mt-0 dark:hover:bg-amber-400 relative lg:text-lg text-xs border py-2 px-4 rounded-full dark:border-gray-100 dark:hover:border-transparent border-third text-third dark:hover:text-black dark:text-gray-100 font-bold hover:bg-third hover:text-white">Visit the website</a>
     </div>
     <div class="relative mt-4">
-      <div class="lg:text-2xl text-xl lg:font-extrabold font-bold font-base my-6">{{data.title}}</div>
+      <div class="lg:text-4xl text-xl lg:font-extrabold font-bold font-base mt-6 lg:mb-10 mb-6 dark:text-white text-black ">{{data.title}}</div>
       <div class="my-4">
         <img :src="data.innerImage" class="mx-auto block" alt="" />
       </div>
