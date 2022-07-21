61273197e362f4e10b828275fd27046a48686a88
DESKTOP-F2VQJ5V\ghkds <ghkdsigm3@gmail.com>
2022-07-21 18:26:14 +0900

    라우터 수정

--- src/components/common/Scroll.vue
+++ src/components/common/Scroll.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <div id="target" @click="handleScroll">
-
-    </div>
+    <ul>
+      <li v-for="(aa, index) in 5" :key="index">
+        <div style="margin:500px 0 500px">
+          ddddddddddddddddddddd
+        </div>
+      </li>
+    </ul>
+    <a @click="scrollTop" v-show="visible" class="bottom-right"> Top </a>
   </div>
 </template>
 
@@ -10,33 +15,106 @@
 export default {
   data(){
     return {
-      isScrollDown: false,
-      scrollTop:0,
-      target: null,
+      visible: false,
     }
   },
   mounted(){
-    this.target = document.querySelector('#target');
-    this.target.addEventListener('scroll', this.handleScroll);
+    
   },
 
-  beforeUnmount(){
-    this.target.removeEventListener('scroll', this.handleScroll)
+  beforeMount() {
+    window.addEventListener("scroll", this.scrollListener);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.scrollListener);
   },
 
   methods:{
-    handleScroll:function(e){
-      this.scrollTop = e.target.scrollTop;
-      if(this.scrollTop > 400){
-        this.isScrollDown = true;
-      } else {
-        this.isScrollDown = false;
-      }
-    }
+    scrollTop() {
+      this.intervalId = setInterval(() => {
+        if (window.pageYOffset === 0) {
+          clearInterval(this.intervalId);
+        }
+        window.scroll(0, window.pageYOffset - 50);
+      }, 2);
+    },
+    scrollListener(e) {
+      this.visible = window.scrollY > 1000;
+    },
+    scrollToElement() {
+      let element = document.getElementById("toEls");
+      element.scrollIntoView({ behavior: "smooth", block: "end" });
+    },
   }
 }
 </script>
 
 <style>
+.scrollIcon {
+    position: absolute;
+    bottom: 50px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    text-align: center;
+    width: 50px;
+    height: 80px;}
+    .line {
+      width: 10px;
+      height: 2px;
+      background: #fff;
+      position: absolute;
+      left: 50%;
+      top: 32px;
+      text-align: center;
+      margin-left: -8px;
+    }
+    .up  span {
+        position: absolute;
+        top: 5px;
+        left: 50%;
+        width: 18px;
+        height: 18px;
+        margin-left: -12px;
+        border-left: 1px solid #fff;
+        border-bottom: 1px solid #fff;
+        -webkit-transform: rotate(135deg);
+        transform: rotate(135deg);
+        -webkit-animation: sdb 3s infinite;
+        animation: sdb 3s infinite;
+        opacity: 0;
+        box-sizing: border-box;      
+      }
 
+      
+    .down span {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      width: 18px;
+      height: 18px;
+      margin-left: -12px;
+      border-left: 1px solid #fff;
+      border-bottom: 1px solid #fff;
+      -webkit-transform: rotate(-45deg);
+      transform: rotate(-45deg);
+      -webkit-animation: sdb 3s infinite;
+      animation: sdb 3s infinite;
+      opacity: 0;
+      box-sizing: border-box;       
+    }
+.bottom-right {
+  position: fixed;
+  bottom: 5%;
+  right: 5%;
+  z-index: 9999;
+  background: #000;
+  color: #fff;
+  padding: 20px;
+  border-radius: 55%;
+  align-items: center;
+  font-weight: 500;
+  font-size: 1.2rem;
+  border: 1px solid #fff;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,18 +11,18 @@ import Work02 from '../pages/works/Work02.vue'
 import WorksDetailNew from '../pages/works/WorksDetailNew.vue'
 import WorksDetailOperation from '../pages/works/WorksDetailOperation.vue'
 import WorksDetailFront from '../pages/works/WorksDetailFront.vue'
-// import Loading from '../components/common/Loading.vue'
+import Scroll from '../components/common/Scroll.vue'
 
-const routes = [
-    { path: '/', name: 'home', component: Home, title: 'HOME', icon: 'fas fa-home fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
-    { path: '/info', name: 'info', component: Info, title: 'INFOMATION', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
-    { path: '/career', name: 'career', component: Career, title: 'CAREER', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
-    { path: '/works', name: 'works', component: Works, title: 'WORKS', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
-    { path: '/works/new/:contentId', name: 'worksDetailNew', component: WorksDetailNew, title: 'WORKSDETAILNEW', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
-    { path: '/works/operation/:contentId', name: 'worksDetailOperation', component: WorksDetailOperation, title: 'WORKSDETAILOPERATION', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
-    { path: '/works/frontend/:contentId', name: 'worksDetailFront', component: WorksDetailFront, title: 'WORKSDETAILFRONT', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
-    { path: '/works/work01', name: 'work01', component: Work01, title: 'WORK01', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
-    { path: '/works/work02', name: 'work02', component: Work02, title: 'WORK02', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
+//const routes = [
+    // { path: '/', name: 'home', component: Home, title: 'HOME', icon: 'fas fa-home fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+    // { path: '/info', name: 'info', component: Info, title: 'INFOMATION', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+    // { path: '/career', name: 'career', component: Career, title: 'CAREER', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+    // { path: '/works', name: 'works', component: Works, title: 'WORKS', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+    // { path: '/works/new/:contentId', name: 'worksDetailNew', component: WorksDetailNew, title: 'WORKSDETAILNEW', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
+    // { path: '/works/operation/:contentId', name: 'worksDetailOperation', component: WorksDetailOperation, title: 'WORKSDETAILOPERATION', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
+    // { path: '/works/frontend/:contentId', name: 'worksDetailFront', component: WorksDetailFront, title: 'WORKSDETAILFRONT', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
+    // { path: '/works/work01', name: 'work01', component: Work01, title: 'WORK01', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
+    // { path: '/works/work02', name: 'work02', component: Work02, title: 'WORK02', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
     // { 
     //     path: '/works', 
     //     name: 'works', 
@@ -47,14 +47,32 @@ const routes = [
     //     icon: 'far fa-bell fa-fw text-2xl', 
     //     meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } 
     // },
-    { path: '/study', name: 'study', component: Study, title: 'STUDY', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
-    { path: '/contact', name: 'contact', component: Contact, title: 'CONTACT', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+    //{ path: '/study', name: 'study', component: Study, title: 'STUDY', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+    //{ path: '/contact', name: 'contact', component: Contact, title: 'CONTACT', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
     //{ path: '/loading', name: 'loading', component: Loading, title: 'LOADING', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true } },
-]
+//]
 
 const router = createRouter({
     history: createWebHistory(),
-    routes,
+    routes: [
+        { path: '/', name: 'home', component: Home, title: 'HOME', icon: 'fas fa-home fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+        { path: '/info', name: 'info', component: Info, title: 'INFOMATION', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+        { path: '/career', name: 'career', component: Career, title: 'CAREER', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+        { path: '/works', name: 'works', component: Works, title: 'WORKS', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+        { path: '/works/new/:contentId', name: 'worksDetailNew', component: WorksDetailNew, title: 'WORKSDETAILNEW', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
+        { path: '/works/operation/:contentId', name: 'worksDetailOperation', component: WorksDetailOperation, title: 'WORKSDETAILOPERATION', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
+        { path: '/works/frontend/:contentId', name: 'worksDetailFront', component: WorksDetailFront, title: 'WORKSDETAILFRONT', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
+        { path: '/works/work01', name: 'work01', component: Work01, title: 'WORK01', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
+        { path: '/works/work02', name: 'work02', component: Work02, title: 'WORK02', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: false, layout: 'SubLayout', requireAuth: true } },
+        { path: '/study', name: 'study', component: Study, title: 'STUDY', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+        { path: '/contact', name: 'contact', component: Contact, title: 'CONTACT', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: true, layout: 'DefaultLayout', requireAuth: true } },
+        
+        { path: '/scroll', name: 'scroll', component: Scroll, title: 'SCROLL', icon: 'far fa-bell fa-fw text-2xl', meta: { isMenu: false, layout: 'DefaultLayout', requireAuth: true } },
+    ],
+    scrollBehavior(to, from, savedPosition) {
+        // always scroll to top
+        return { top: 0 }
+    },
 })
 
 router.beforeEach((to, from, next) => {
