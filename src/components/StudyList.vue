73c0dd4da0fd2982c90a808c8c9281cdb858196e
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-08-20 17:35:05 +0900

    study 반응형 수정

--- src/components/StudyList.vue
+++ src/components/StudyList.vue
@@ -83,7 +83,7 @@
                 <div v-for="item in tweets" :key="item.id">
                     <div class="text-left dark:text-white text-black py-10 border-t dark:border-gray-600 border-gray-300">
                         <p class="lg:mb-0 mb-2 text-third dark:text-fifth dark:hover:text-fifthHover font-light lg:text-sm text-sm">{{ item.category }}</p>
-                        <router-link :to="`/study/board/${item.id}`" :item="item" :currentUser="currentUser" class="block lg:pb-0 pb-4 hover:underline dark:hover:text-primary hover:text-third">
+                        <router-link :to="`/study/board/${item.id}`" :item="item" :currentUser="currentUser" class="block pb-4 pt-1 hover:underline dark:hover:text-primary hover:text-third">
                             <h2 class="text-lg">{{ item.board_body }}</h2>
                             <p class="text-lg font-thin">{{ item.body }}</p>
                         </router-link>
