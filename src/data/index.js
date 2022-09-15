ad4dc3f1e8254b072852db4ee840e2da0136d256
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-09-15 21:27:12 +0900

    디자인 이미지 추가

--- src/data/index.js
+++ src/data/index.js
@@ -32,7 +32,7 @@ export default {
         user_id: 1,
         title: "유벤투스 vs 팀케이리그 전광판 구축 사업",
         context: "전광판 송출 디자인 작업",
-        created_at: "2018-09-11",
+        created_at: "2019.06. - 19.07.",
         party: '100%',
         content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d01.gif"
       },
@@ -50,16 +50,16 @@ export default {
         user_id: 3,
         title: "스카이워커스 배구단 전광판 구축 사업",
         context: "전광판 송출 디자인 작업",
-        created_at: "2018-09-11",
+        created_at: "2019.09. - 19.10.",
         party: '100%',
         content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d03.gif"
       },
       {
         content_id: 4,
         user_id: 4,
-        title: "대한체육회 타임캡슐 공모전",
+        title: "대한체육회_대한체육회 100주년 기념 슬로건 공모 응모시스템 구축·운영",
         context: "대국민 대상 공모 사이트 및 포스터 디자인 작업",
-        created_at: "2018-09-11",
+        created_at: "2018.05. - 18.06.",
         party: '100%',
         content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d04.jpg"
       },
@@ -77,7 +77,7 @@ export default {
         user_id: 6,
         title: "경기도시장상권진흥원 소상공인 온라인 판매 유치 사업",
         context: "소상공인 대상 온라인 판매 유치 팜플릿 및 레플릿 제작",
-        created_at: "2018-09-11",
+        created_at: "2021.06.",
         party: '100%',
         content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d06.png"
       },
@@ -86,9 +86,27 @@ export default {
         user_id: 7,
         title: "자사 플랫폼",
         context: "데이터센터 플랫폼 디자인 작업",
-        created_at: "2018-09-11",
+        created_at: "2020.03.",
         party: '100%',
         content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d07.gif"
+      },
+      {
+        content_id: 8,
+        user_id: 8,
+        title: "대한체육회_대한체육회 100주년 기념 슬로건 공모 응모시스템 구축·운영",
+        context: "대한체육회 미래100년 후대에게 보내는 타임캡슐 '손편지' 공모 포스터 제작",
+        created_at: "2018.05. - 18.06",
+        party: '100%',
+        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d08.jpg"
+      },
+      {
+        content_id: 7,
+        user_id: 7,
+        title: "제주유나이티드 내손안의전광판 플랫폼 운영 고도화 사업",
+        context: "내손안의 전광판 컨텐츠 제작",
+        created_at: "2021.03. - 21.09",
+        party: '100%',
+        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d09.jpg"
       }
     ],
     // Comment: [
