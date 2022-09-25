c43ba80822147504907bc6f2b2307ed89f2942c3
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-09-25 15:11:09 +0900

    사파리 랙 확인

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './index.css'
 import router from './router'
 import store from './store'
-import magicmouse from "magicmouse.js";
+//import magicmouse from 'magicmouse.js';
 import swal from 'sweetalert'
 
-createApp(App).use(magicmouse).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
