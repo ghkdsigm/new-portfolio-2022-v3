7bbd99c8fdbd4c6346b3f68293cfc6dbe01c10bf
ghkdsigm3 <ghkdsigm3@gmail.com>
2023-04-05 20:20:26 +0900

    education

--- src/components/Introduce.vue
+++ src/components/Introduce.vue
@@ -71,9 +71,9 @@
             >
           </li>
           <li class="py-1 lg:text-lg text-base flex">
-            <span class="font-medium">2022.01.</span>
+            <span class="font-medium">~2022.06.</span>
             <span class="pl-4"
-              >패스트캠퍼스 프론트엔드 초격차 패키지 온라인 교육 과정 수료</span
+              >패스트캠퍼스 프론트엔드 초격차 패키지 온라인 교육 과정중</span
             >
           </li>
           <li class="py-1 lg:text-lg text-base flex">
