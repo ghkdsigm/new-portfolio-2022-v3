897464cdd231459bc62c7a810b67b1b1e48b8df1
ghkdsigm3 <ghkdsigm3@gmail.com>
2023-05-06 00:55:23 +0900

    이미지 로컬로 위치 변경

--- src/data/index.js
+++ src/data/index.js
@@ -1,173 +1,175 @@
 export default {
-    // User: [
-    //   {
-    //     user_id: 1,
-    //     name: "인성",
-    //     created_at: "2018-09-11"
-    //   },
-    //   {
-    //     user_id: 2,
-    //     name: "탁우",
-    //     created_at: "2018-09-12"
-    //   },
-    //   {
-    //     user_id: 3,
-    //     name: "준우",
-    //     created_at: "2018-09-13"
-    //   },
-    //   {
-    //     user_id: 4,
-    //     name: "채우",
-    //     created_at: "2018-09-14"
-    //   },
-    //   {
-    //     user_id: 5,
-    //     name: "예나",
-    //     created_at: "2018-09-15"
-    //   }
-    // ],
-    Content: [
-      {
-        content_id: 1,
-        user_id: 1,
-        title: "유벤투스 vs 팀케이리그 전광판 구축 사업",
-        context: "전광판 송출 디자인 작업",
-        created_at: "2019.06. - 19.07.",
-        party: '100%',
-        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d01.gif"
-      },
-      {
-        content_id: 2,
-        user_id: 2,
-        title: "대한체육회 스포츠영웅 선정 국민지지도 조사",
-        context: "대한체육회 국민지지도조사 팝업, 배너, 홈페이지 디자인 작업",
-        created_at: "2016, ,17, 18, 19, 20, 21",
-        party: '100%',
-        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d02.png"
-      },
-      {
-        content_id: 3,
-        user_id: 3,
-        title: "스카이워커스 배구단 전광판 구축 사업",
-        context: "전광판 송출 디자인 작업",
-        created_at: "2019.09. - 19.10.",
-        party: '100%',
-        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d03.gif"
-      },
-      {
-        content_id: 4,
-        user_id: 4,
-        title: "대한체육회_대한체육회 100주년 기념 슬로건 공모 응모시스템 구축·운영",
-        context: "대국민 대상 공모 사이트 및 포스터 디자인 작업",
-        created_at: "2018.05. - 18.06.",
-        party: '100%',
-        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d04.jpg"
-      },
-      {
-        content_id: 5,
-        user_id: 5,
-        title: "대한체육회 스포츠영웅 후보자 추천",
-        context: "대한체육회 후보자추천 팝업, 배너, 홈페이지 디자인 작업",
-        created_at: "2016, ,17, 18, 19, 20, 21",
-        party: '100%',
-        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d05.jpg"
-      },
-      {
-        content_id: 6,
-        user_id: 6,
-        title: "경기도시장상권진흥원 소상공인 온라인 판매 유치 사업",
-        context: "소상공인 대상 온라인 판매 유치 팜플릿 및 레플릿 제작",
-        created_at: "2021.06.",
-        party: '100%',
-        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d06.png"
-      },
-      {
-        content_id: 7,
-        user_id: 7,
-        title: "자사 플랫폼",
-        context: "데이터센터 플랫폼 디자인 작업",
-        created_at: "2020.03.",
-        party: '100%',
-        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d07.gif"
-      },
-      {
-        content_id: 8,
-        user_id: 8,
-        title: "대한체육회_대한체육회 100주년 기념 슬로건 공모 응모시스템 구축·운영",
-        context: "대한체육회 미래100년 후대에게 보내는 타임캡슐 '손편지' 공모 포스터 제작",
-        created_at: "2018.05. - 18.06",
-        party: '100%',
-        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d08.jpg"
-      },
-      {
-        content_id: 7,
-        user_id: 7,
-        title: "제주유나이티드 내손안의전광판 플랫폼 운영 고도화 사업",
-        context: "내손안의 전광판 컨텐츠 제작",
-        created_at: "2021.03. - 21.09",
-        party: '100%',
-        content_url: "https://raw.githubusercontent.com/ghkdsigm/new-portfolio-2022/main/src/assets/images/design/d09.jpg"
-      }
-    ],
-    // Comment: [
-    //   {
-    //     comment_id: 1,
-    //     content_id: 2,
-    //     user_id: 1,
-    //     context: "안녕하세요1",
-    //     created_at: "2018-09-15",
-    //     updated_at: null
-    //   },
-    //   {
-    //     comment_id: 2,
-    //     content_id: 1,
-    //     user_id: 1,
-    //     context: "안녕하세요2",
-    //     created_at: "2018-09-12",
-    //     updated_at: null
-    //   },
-    //   {
-    //     comment_id: 3,
-    //     content_id: 3,
-    //     user_id: 3,
-    //     context: "안녕하세요3",
-    //     created_at: "2018-09-15",
-    //     updated_at: null
-    //   },
-    //   {
-    //     comment_id: 4,
-    //     content_id: 3,
-    //     user_id: 1,
-    //     context: "안녕하세요4",
-    //     created_at: "2018-09-19",
-    //     updated_at: null
-    //   },
-    //   {
-    //     comment_id: 5,
-    //     content_id: 3,
-    //     user_id: 2,
-    //     context: "안녕하세요6",
-    //     created_at: "2018-09-20",
-    //     updated_at: null
-    //   }
-    // ],
-    // SubComment: [
-    //   {
-    //     subcomment_id: 1,
-    //     comment_id: 3,
-    //     user_id: 3,
-    //     context: "서브 댓글 1",
-    //     created_at: "2018-09-15",
-    //     updated_at: null
-    //   },
-    //   {
-    //     subcomment_id: 2,
-    //     comment_id: 3,
-    //     user_id: 1,
-    //     context: "서브 댓글 2",
-    //     created_at: "2018-09-16",
-    //     updated_at: null
-    //   }
-    // ]
-  };
-  
+  // User: [
+  //   {
+  //     user_id: 1,
+  //     name: "인성",
+  //     created_at: "2018-09-11"
+  //   },
+  //   {
+  //     user_id: 2,
+  //     name: "탁우",
+  //     created_at: "2018-09-12"
+  //   },
+  //   {
+  //     user_id: 3,
+  //     name: "준우",
+  //     created_at: "2018-09-13"
+  //   },
+  //   {
+  //     user_id: 4,
+  //     name: "채우",
+  //     created_at: "2018-09-14"
+  //   },
+  //   {
+  //     user_id: 5,
+  //     name: "예나",
+  //     created_at: "2018-09-15"
+  //   }
+  // ],
+  Content: [
+    {
+      content_id: 1,
+      user_id: 1,
+      title: '유벤투스 vs 팀케이리그 전광판 구축 사업',
+      context: '전광판 송출 디자인 작업',
+      created_at: '2019.06. - 19.07.',
+      party: '100%',
+      content_url: '/src/assets/images/design/d01.gif',
+    },
+    {
+      content_id: 2,
+      user_id: 2,
+      title: '대한체육회 스포츠영웅 선정 국민지지도 조사',
+      context: '대한체육회 국민지지도조사 팝업, 배너, 홈페이지 디자인 작업',
+      created_at: '2016, ,17, 18, 19, 20, 21',
+      party: '100%',
+      content_url: '/src/assets/images/design/d02.png',
+    },
+    {
+      content_id: 3,
+      user_id: 3,
+      title: '스카이워커스 배구단 전광판 구축 사업',
+      context: '전광판 송출 디자인 작업',
+      created_at: '2019.09. - 19.10.',
+      party: '100%',
+      content_url: '/src/assets/images/design/d03.gif',
+    },
+    {
+      content_id: 4,
+      user_id: 4,
+      title:
+        '대한체육회_대한체육회 100주년 기념 슬로건 공모 응모시스템 구축·운영',
+      context: '대국민 대상 공모 사이트 및 포스터 디자인 작업',
+      created_at: '2018.05. - 18.06.',
+      party: '100%',
+      content_url: '/src/assets/images/design/d04.jpg',
+    },
+    {
+      content_id: 5,
+      user_id: 5,
+      title: '대한체육회 스포츠영웅 후보자 추천',
+      context: '대한체육회 후보자추천 팝업, 배너, 홈페이지 디자인 작업',
+      created_at: '2016, ,17, 18, 19, 20, 21',
+      party: '100%',
+      content_url: '/src/assets/images/design/d05.jpg',
+    },
+    {
+      content_id: 6,
+      user_id: 6,
+      title: '경기도시장상권진흥원 소상공인 온라인 판매 유치 사업',
+      context: '소상공인 대상 온라인 판매 유치 팜플릿 및 레플릿 제작',
+      created_at: '2021.06.',
+      party: '100%',
+      content_url: '/src/assets/images/design/d06.png',
+    },
+    {
+      content_id: 7,
+      user_id: 7,
+      title: '자사 플랫폼',
+      context: '데이터센터 플랫폼 디자인 작업',
+      created_at: '2020.03.',
+      party: '100%',
+      content_url: '/src/assets/images/design/d07.gif',
+    },
+    {
+      content_id: 8,
+      user_id: 8,
+      title:
+        '대한체육회_대한체육회 100주년 기념 슬로건 공모 응모시스템 구축·운영',
+      context:
+        "대한체육회 미래100년 후대에게 보내는 타임캡슐 '손편지' 공모 포스터 제작",
+      created_at: '2018.05. - 18.06',
+      party: '100%',
+      content_url: '/src/assets/images/design/d08.jpg',
+    },
+    {
+      content_id: 7,
+      user_id: 7,
+      title: '제주유나이티드 내손안의전광판 플랫폼 운영 고도화 사업',
+      context: '내손안의 전광판 컨텐츠 제작',
+      created_at: '2021.03. - 21.09',
+      party: '100%',
+      content_url: '/src/assets/images/design/d09.jpg',
+    },
+  ],
+  // Comment: [
+  //   {
+  //     comment_id: 1,
+  //     content_id: 2,
+  //     user_id: 1,
+  //     context: "안녕하세요1",
+  //     created_at: "2018-09-15",
+  //     updated_at: null
+  //   },
+  //   {
+  //     comment_id: 2,
+  //     content_id: 1,
+  //     user_id: 1,
+  //     context: "안녕하세요2",
+  //     created_at: "2018-09-12",
+  //     updated_at: null
+  //   },
+  //   {
+  //     comment_id: 3,
+  //     content_id: 3,
+  //     user_id: 3,
+  //     context: "안녕하세요3",
+  //     created_at: "2018-09-15",
+  //     updated_at: null
+  //   },
+  //   {
+  //     comment_id: 4,
+  //     content_id: 3,
+  //     user_id: 1,
+  //     context: "안녕하세요4",
+  //     created_at: "2018-09-19",
+  //     updated_at: null
+  //   },
+  //   {
+  //     comment_id: 5,
+  //     content_id: 3,
+  //     user_id: 2,
+  //     context: "안녕하세요6",
+  //     created_at: "2018-09-20",
+  //     updated_at: null
+  //   }
+  // ],
+  // SubComment: [
+  //   {
+  //     subcomment_id: 1,
+  //     comment_id: 3,
+  //     user_id: 3,
+  //     context: "서브 댓글 1",
+  //     created_at: "2018-09-15",
+  //     updated_at: null
+  //   },
+  //   {
+  //     subcomment_id: 2,
+  //     comment_id: 3,
+  //     user_id: 1,
+  //     context: "서브 댓글 2",
+  //     created_at: "2018-09-16",
+  //     updated_at: null
+  //   }
+  // ]
+};
