3eb8395a8d666c77f049563e6649943c79208c71
DONGWHA\D2301202 <sh.hwang@dongwha.com>
2025-03-13 10:13:39 +0900

    update

--- src/components/Introduce.vue
+++ src/components/Introduce.vue
@@ -1,135 +1,115 @@
 <template>
-  <div class="self-center lg:mt-80 mt-32 text-left">
-    <h2
-      class="comment lg:text-6xl text-4xl lg:text-left text-center font-extrabold leading-tight dark:text-secondary text-secondary-dark"
-    >
-      Various<br />Work Experiences<br />For your Brand
-    </h2>
-    <div class="my-14 lg:grid block grid-cols-5 justify-items-center">
-      <!--기본정보(사진)-->
-      <div class="col-span-2">
-        <img
-          src="@/assets/images/face.png"
-          class="lg:m-0 mx-auto"
-          alt="황승현의 이미지"
-        />
-        <!--기본정보-->
-      </div>
-      <!--기본정보(정보)-->
-      <div
-        class="col-span-3 flex items-center dark:text-secondary text-secondary-dark lg:mt-0 mt-12"
-      >
-        <div>
-          <h3 class="lg:text-4xl text-3xl dark:text-secondary font-bold">
-            황 승 현
-          </h3>
-          <p class="lg:text-xl text-xl py-2 text-third dark:text-third-dark">
-            HWANG SEUNG HYUN
-          </p>
-          <ul class="font-light lg:text-lg text-base">
-            <li class="py-1">1990.01.13.</li>
-            <li class="py-1">Yangcheon-gu, Seoul, Republic of Korea</li>
-            <li class="py-1">ghkdsigm3@naver.com / ghkdsigm3@gmail.com</li>
-            <li class="py-1 flex">
-              +82 10.9406-3935
-              <!-- /
+	<div class="self-center lg:mt-80 mt-32 text-left">
+		<h2
+			class="comment lg:text-6xl text-4xl lg:text-left text-center font-extrabold leading-tight dark:text-secondary text-secondary-dark"
+		>
+			Various<br />Work Experiences<br />For your Brand
+		</h2>
+		<div class="my-14 lg:grid block grid-cols-5 justify-items-center">
+			<!--기본정보(사진)-->
+			<div class="col-span-2">
+				<img
+					src="@/assets/images/face.png"
+					class="lg:m-0 mx-auto"
+					alt="황승현의 이미지"
+				/>
+				<!--기본정보-->
+			</div>
+			<!--기본정보(정보)-->
+			<div class="col-span-3 flex items-center dark:text-secondary text-secondary-dark lg:mt-0 mt-12">
+				<div>
+					<h3 class="lg:text-4xl text-3xl dark:text-secondary font-bold">황 승 현</h3>
+					<p class="lg:text-xl text-xl py-2 text-third dark:text-third-dark">HWANG SEUNG HYUN</p>
+					<ul class="font-light lg:text-lg text-base">
+						<li class="py-1">1990.01.13.</li>
+						<li class="py-1">Yangcheon-gu, Seoul, Republic of Korea</li>
+						<li class="py-1">ghkdsigm3@naver.com / ghkdsigm3@gmail.com</li>
+						<li class="py-1 flex">
+							+82 10.9406-3935
+							<!-- /
               <a href="javascript:void(0);" id="resume" class="flex">
                 <img src="" alt="이력서">
                 이력서 & 경력기술서.pdf
               </a> -->
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="lg:mt-4 block dark:text-secondary text-secondary-dark">
-      <!--기본정보(이력)-->
-      <h4 class="lg:text-2xl text-base mb-10 font-light">
-        <b class="lg:text-4xl text-2xl block font-extrabold mb-3"
-          >'적자생존은 만고불변의 진리'</b
-        >
-        주어진 어떠한 역할과 환경에서도<br />
-        동료들과 함께 더 나은 길을 만들어 나아가는 사람이 되고자 합니다.
-      </h4>
-      <div class="career mb-6">
-        <h5 class="font-bold lg:text-3xl text-xl dark:text-primary text-third">
-          Educations
-        </h5>
-        <ul>
-          <li class="py-1 lg:text-lg text-base flex">
-            <span class="font-medium">2008.03.</span>
-            <span class="pl-4">서울양정고등학교 졸업</span>
-          </li>
-          <li class="py-1 lg:text-lg text-base flex">
-            <span class="font-medium">2016.01.</span>
-            <span class="pl-4">경기대학교 경영학과 졸업</span>
-          </li>
-          <li class="py-1 lg:text-lg text-base flex">
-            <span class="font-medium">2016.12.</span>
-            <span class="pl-4"
-              >NCS 기반 반응형 디지털웹(웹표준&웹접근성) 과정 수료</span
-            >
-          </li>
-          <li class="py-1 lg:text-lg text-base flex">
-            <span class="font-medium">2022.01.</span>
-            <span class="pl-4"
-              >패스트캠퍼스 프론트엔드 초격차 패키지 온라인 교육 과정 수료</span
-            >
-          </li>
-          <li class="py-1 lg:text-lg text-base flex">
-            <span class="font-medium">2023.06 ~</span>
-            <span class="pl-4"
-              >패스트캠퍼스 Node.js 웹 프로그래밍 초격차 패키지 Online</span
-            >
-          </li>
-        </ul>
-      </div>
-      <div class="career mb-6">
-        <h5 class="font-bold lg:text-3xl text-xl dark:text-primary text-third">
-          Licenses
-        </h5>
-        <ul>
-          <li class="py-1 lg:text-lg text-base flex">
-            <span class="font-medium">2008.08.</span>
-            <span class="pl-4">자동차1종보통면허 취득</span>
-          </li>
-          <li class="py-1 lg:text-lg text-base flex">
-            <span class="font-medium">2012.03.</span>
-            <span class="pl-4">텔레마케팅관리사 취득</span>
-          </li>
-          <li class="py-1 lg:text-lg text-base flex">
-            <span class="font-medium">2016.01.</span>
-            <span class="pl-4">한국사능력검정 1급 취득</span>
-          </li>
-          <li class="py-1 lg:text-lg text-base flex">
-            <span class="font-medium">2019.08.</span>
-            <span class="pl-4">정보처리기사 취득</span>
-          </li>
-        </ul>
-      </div>
-      <div class="career mb-6">
-        <h5 class="font-bold lg:text-3xl text-xl dark:text-primary text-third">
-          Capability
-        </h5>
-        <ul>
-          <li class="py-1 lg:text-lg text-base">
-            <span class="font-light">UI · UX Design</span>
-          </li>
-          <li class="py-1 lg:text-lg text-base">
-            <span class="font-light">UI · UX Development</span>
-          </li>
-          <li class="py-1 lg:text-lg text-base">
-            <span class="font-light">Front-end Development</span>
-          </li>
-          <li class="py-1 lg:text-lg text-base">
-            <span class="font-light">Making Proposal</span>
-          </li>
-          <li class="py-1 lg:text-lg text-base">
-            <span class="font-light">Service Communication</span>
-          </li>
-        </ul>
-      </div>
-      <!-- <div class="career mb-6">
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="lg:mt-4 block dark:text-secondary text-secondary-dark">
+			<!--기본정보(이력)-->
+			<h4 class="lg:text-2xl text-base mb-10 font-light">
+				<b class="lg:text-4xl text-2xl block font-extrabold mb-3">'적자생존은 만고불변의 진리'</b>
+				주어진 어떠한 역할과 환경에서도<br />
+				동료들과 함께 더 나은 길을 만들어 나아가는 사람이 되고자 합니다.
+			</h4>
+			<div class="career mb-6">
+				<h5 class="font-bold lg:text-3xl text-xl dark:text-primary text-third">Educations</h5>
+				<ul>
+					<li class="py-1 lg:text-lg text-base flex">
+						<span class="font-medium">2008.03.</span>
+						<span class="pl-4">서울양정고등학교 졸업</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base flex">
+						<span class="font-medium">2016.01.</span>
+						<span class="pl-4">경기대학교 경영학과 졸업</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base flex">
+						<span class="font-medium">2016.12.</span>
+						<span class="pl-4">NCS 기반 반응형 디지털웹(웹표준&웹접근성) 과정 수료</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base flex">
+						<span class="font-medium">2022.01.</span>
+						<span class="pl-4">패스트캠퍼스 프론트엔드 초격차 패키지 온라인 교육 과정 수료</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base flex">
+						<span class="font-medium">2025.03 ~</span>
+						<span class="pl-4">패스트캠퍼스 Node.js 웹 프로그래밍 초격차 패키지 Online</span>
+					</li>
+				</ul>
+			</div>
+			<div class="career mb-6">
+				<h5 class="font-bold lg:text-3xl text-xl dark:text-primary text-third">Licenses</h5>
+				<ul>
+					<li class="py-1 lg:text-lg text-base flex">
+						<span class="font-medium">2008.08.</span>
+						<span class="pl-4">자동차1종보통면허 취득</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base flex">
+						<span class="font-medium">2012.03.</span>
+						<span class="pl-4">텔레마케팅관리사 취득</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base flex">
+						<span class="font-medium">2016.01.</span>
+						<span class="pl-4">한국사능력검정 1급 취득</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base flex">
+						<span class="font-medium">2019.08.</span>
+						<span class="pl-4">정보처리기사 취득</span>
+					</li>
+				</ul>
+			</div>
+			<div class="career mb-6">
+				<h5 class="font-bold lg:text-3xl text-xl dark:text-primary text-third">Capability</h5>
+				<ul>
+					<li class="py-1 lg:text-lg text-base">
+						<span class="font-light">UI · UX Design</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base">
+						<span class="font-light">UI · UX Development</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base">
+						<span class="font-light">Front-end Development</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base">
+						<span class="font-light">Making Proposal</span>
+					</li>
+					<li class="py-1 lg:text-lg text-base">
+						<span class="font-light">Service Communication</span>
+					</li>
+				</ul>
+			</div>
+			<!-- <div class="career mb-6">
         <h5 class="font-bold lg:text-3xl text-xl">Skills / Tools</h5>
         <ul class="mytools">
           <li class="py-1 lg:text-lg text-base">
@@ -146,37 +126,36 @@
           </li>
         </ul>
       </div> -->
-    </div>
-  </div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style lang="scss" scoped>
 .comment {
-  font-family: 'Noto Sans KR';
-  letter-spacing: 0px;
-  -webkit-transition: all 0.3s ease 0.31s;
-  transition: all 0.3s ease 0.31s;
-  font-family: 'Exo', sans-serif;
-  &::before {
-    content: '';
-    transition: left 0.2s cubic-bezier(0.47, 0, 0.745, 0.715),
-      width 0.15s cubic-bezier(0.47, 0, 0.745, 0.715),
-      opacity 0.2s cubic-bezier(0.55, 0.055, 0.675, 0.19);
-    height: 1px;
-    position: absolute;
-    bottom: -20px;
-    width: 0;
-    height: 0.14em;
-    bottom: 0;
-    left: 0;
-    opacity: 0.5;
-  }
+	font-family: 'Noto Sans KR';
+	letter-spacing: 0px;
+	-webkit-transition: all 0.3s ease 0.31s;
+	transition: all 0.3s ease 0.31s;
+	font-family: 'Exo', sans-serif;
+	&::before {
+		content: '';
+		transition: left 0.2s cubic-bezier(0.47, 0, 0.745, 0.715), width 0.15s cubic-bezier(0.47, 0, 0.745, 0.715),
+			opacity 0.2s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+		height: 1px;
+		position: absolute;
+		bottom: -20px;
+		width: 0;
+		height: 0.14em;
+		bottom: 0;
+		left: 0;
+		opacity: 0.5;
+	}
 }
 .comment_away {
-  color: #000;
+	color: #000;
 }
 </style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,56 +1,44 @@
 <template>
-  <div class="lg:mt-4 block dark:text-secondary text-secondary-dark text-left">
-    <h4 class="lg:text-xl text-base mb-10 font-light">
-      <b class="lg:text-4xl text-2xl block font-extrabold mb-2">EXPERTISE</b>
-      실무에 활용하였고 또 계속해서 배움을 실천하고있는 언어이자
-      도구들입니다.<br />
-      사용하고있는 언어 및 기술들과 더불어<br />
-      나아가 컴퓨터 공학 & 웹브라우저 & 알고리즘 등<br />
-      다양한 지식들의 배움을 꾸준히 실천하여<br />
-      더욱 폭넓게 확장된 업무 수행 능력을 기를것입니다.
-    </h4>
-    <div class="career mb-6">
-      <h5 class="font-bold lg:text-3xl text-xl dark:text-primary text-third">
-        Skills
-      </h5>
-      <ul class="mytools">
-        <li class="py-1 lg:text-lg text-base">
-          <span class="font-light"
-            >Html5 / Css3 / Scss / Tailwind.css / Bootstrap / Mudblazor</span
-          >
-        </li>
-        <li class="py-1 lg:text-lg text-base">
-          <span class="font-light"
-            >Javascript / Jquery / Vue2,3 / Nuxt.js / Firebase / Typescript(in
-            progress)
-          </span>
-        </li>
-        <li class="py-1 lg:text-lg text-base">
-          <span class="font-light"
-            >PWA / I18N / Serial Commnunication
-          </span>
-        </li>
-        <li class="py-1 lg:text-lg text-base">
-          <span class="font-light"
-            >Git / Github / Gitlab / Svn / Netlify / Heroku / Gulp
-          </span>
-        </li>
-      </ul>
-    </div>
-    <div class="career mb-6">
-      <h5 class="font-bold lg:text-3xl text-xl dark:text-primary text-third">
-        Tools
-      </h5>
-      <ul class="mytools">
-        <li class="py-1 lg:text-lg text-base">
-          <span class="font-light"
-            >VSCode / Visual Studio / Eclipse / Powerpoint / Excel / Photoshop /
-            Figma / Zeplin
-          </span>
-        </li>
-      </ul>
-    </div>
-    <!-- <div class="career mb-6">
+	<div class="lg:mt-4 block dark:text-secondary text-secondary-dark text-left">
+		<h4 class="lg:text-xl text-base mb-10 font-light">
+			<b class="lg:text-4xl text-2xl block font-extrabold mb-2">EXPERTISE</b>
+			실무에 활용하였고 또 계속해서 배움을 실천하고있는 언어이자 도구들입니다.<br />
+			사용하고있는 언어 및 기술들과 더불어<br />
+			나아가 컴퓨터 공학 & 웹브라우저 & 알고리즘 등<br />
+			다양한 지식들의 배움을 꾸준히 실천하여<br />
+			더욱 폭넓게 확장된 업무 수행 능력을 기를것입니다.
+		</h4>
+		<div class="career mb-6">
+			<h5 class="font-bold lg:text-3xl text-xl dark:text-primary text-third">Skills</h5>
+			<ul class="mytools">
+				<li class="py-1 lg:text-lg text-base">
+					<span class="font-light">Html5 / Css3 / Scss / Tailwind.css / Bootstrap / Mudblazor</span>
+				</li>
+				<li class="py-1 lg:text-lg text-base">
+					<span class="font-light"
+						>Javascript / Jquery / Vue2,3 / Nuxt2.js / Nuxt3.js / Firebase / Typescript(in progress)
+					</span>
+				</li>
+				<li class="py-1 lg:text-lg text-base">
+					<span class="font-light">PWA / I18N / Serial Commnunication </span>
+				</li>
+				<li class="py-1 lg:text-lg text-base">
+					<span class="font-light">Git / Github / Gitlab / Svn / Netlify / Heroku / Gulp /Aws </span>
+				</li>
+			</ul>
+		</div>
+		<div class="career mb-6">
+			<h5 class="font-bold lg:text-3xl text-xl dark:text-primary text-third">Tools</h5>
+			<ul class="mytools">
+				<li class="py-1 lg:text-lg text-base">
+					<span class="font-light"
+						>VSCode / Visual Studio / Eclipse / Powerpoint / Excel / Photoshop / Figma / Zeplin / Postman /
+						Swagger
+					</span>
+				</li>
+			</ul>
+		</div>
+		<!-- <div class="career mb-6">
       <ul>
         <li>
           <img src="" alt="" />
@@ -78,11 +66,11 @@
         </li>
       </ul>
     </div> -->
-  </div>
+	</div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style scoped></style>
